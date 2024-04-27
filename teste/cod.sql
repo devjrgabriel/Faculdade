258e814a354bcd978ea43cf8d13971588458cdc3
devjrgabriel <devjrgabrielsouza@gmail.com>
2024-04-26 22:15:56 -0300

    att

---
+++ teste/cod.sql
@@ -0,0 +1,16 @@
+create database AulasCampoReal;
+
+USE AulasCampoReal;
+
+create table if not exists cliente(
+	cpfcliente int not null auto_increment,
+    nome varchar(45) not null,
+    endereco varchar(45),
+    primary key (cpfcliente)
+);
+insert into cliente values ( '12345','Gabriel','rua um');
+
+insert into cliente (cpfcliente,nome,endereco) values ( '45678','Maria','rua dois');
+
+select * from cliente;
+select * from cliente where cpfcliente = '12345' or nome = 'g' ;
