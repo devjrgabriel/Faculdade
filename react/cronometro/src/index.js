8ff7d9d9066003b87838c0e8310d0b45bb14579e
devjrgabriel <devjrgabrielsouza@gmail.com>
2024-05-15 20:23:39 -0300

    att

--- react/cronometro/src/index.js
+++ react/cronometro/src/index.js
@@ -1,5 +1,11 @@
-import React, { Component} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import './index.css';
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
