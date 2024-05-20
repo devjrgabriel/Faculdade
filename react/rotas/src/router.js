54991057c32baa074e2892b495e490b67c30e62c
devjrgabriel <devjrgabrielsouza@gmail.com>
2024-05-20 19:41:02 -0300

    att

---
+++ react/rotas/src/router.js
@@ -0,0 +1,8 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from './pages/home'
+import Contato from './pages/contato'
+import Erro from './pages/erro'
+import Produto from "./pages/produto"
+import Sobre from './pages/Sobre'
+import Header from'./components/Header'
