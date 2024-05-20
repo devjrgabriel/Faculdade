ed447bd86f1efa58fd288e5c75e7d046566ca1f6
devjrgabriel <devjrgabrielsouza@gmail.com>
2024-05-20 19:51:46 -0300

    att

--- react/rotas/src/router.js
+++ react/rotas/src/router.js
@@ -5,4 +5,19 @@ import Contato from './pages/contato'
 import Erro from './pages/erro'
 import Produto from "./pages/produto"
 import Sobre from './pages/Sobre'
-import Header from'./components/Header'
+import Header from './components/Header'
+
+export default function RouteApp(){
+    return (
+        <BrowserRouter>
+            <Header/>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/Sobre" element={<Sobre/>}/>
+                <Route path="/Contato" element={<Contato/>}/>
+                <Route path="/Produto/:id" element={<Produto/>}/>
+                <Route path="*" element={<Erro/>}/>
+            </Routes>
+        </BrowserRouter>
+    )
+}
